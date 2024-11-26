8f48bdefa01465f4ba7e751e9ba7754b139b3c69
nima <irspidi@gmail.com>
2024-11-26 11:22:40 +0330

    aaaaa

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-let user = [
-  {
-    id: 1,
-    fname: "Amir hamze",
-    lastname: "tahmasbi",
-    age: 18,
-    gender: "M",
-  },
-  {
-    id: 2,
-    fname: "nima",
-    lastname: "shah baba",
-    age: 22,
-    gender: "M",
-  },
-  {
-    id: 3,
-    fname: "zahra",
-    lastname: "tajik",
-    age: 19,
-    gender: "F",
-  },
-  {
-    id: 4,
-    fname: "Razieh",
-    lastname: "sabekiyan",
-    age: 44,
-    gender: "F",
-  },
-];
+let [name, , age, ...number] = ["nima ", "tahmasbi", 18, 1, 2, 3, 4];
 
-let q = user.filter((user) => user.age > 20);
-console.log(q);
+console.log(name, age, number);
